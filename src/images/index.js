d22329c9e20fecda8bf58cee836dde68160f5a3c
Tharcio Sampaio <tatosampaiolima@gmail.com>
2022-01-17 12:14:27 -0300

    Basic structure of the Header component, Images folder and data file.

---
+++ src/images/index.js
@@ -0,0 +1,11 @@
+import perfilImage from './perfil.jpg'
+import trybeImage from './trybe.jpg';
+import githubImage from './github.png';
+import qbGif from './qb.gif';
+
+export {
+  perfilImage,
+  trybeImage,
+  githubImage,
+  qbGif,
+};
